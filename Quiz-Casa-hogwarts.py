d901a77f8aff21f688eb27392804d40f0196648d
vdma19 <133059789+vdma19@users.noreply.github.com>
2024-06-09 08:44:57 -0600

    Add files via upload

--- Quiz-Casa-hogwarts.py
+++ Quiz-Casa-hogwarts.py
@@ -1,49 +1,64 @@
-def Q1 ():
-    print ("Te gusta el amanecer o el anochecer?")
-    print ("1. Amanecer")
-    print ("2. Anochecer")
-    respuesta1=int(input("Su respuesta es: "))
-    if respuesta1==1:
-        print( "Gryffindor y Ravenclaw +1 ")
-    elif respuesta1==2:
-        print ("Hufflepuff y Slytherin +1")
-    else:
-        print("Respuesta invalida")
-Q1()
-def Q2 ():
-    print ("Cuando muera, quiero que la gente me recuerde como: ")
-    print ("1. El bien")
-    print ("2. El grande")
-    print ("3. El sabio")
-    print ("4. El audaz")
-    respuesta2=int(input("Su respuesta es:  "))
-    if respuesta2==1:
-        print("Hufflepuff +2")
-    elif respuesta2==2:
-        print("Slytherin +2")
-    elif respuesta2==3:
-        print("Ravenclaw +2")
-    elif respuesta2==4:
-        print("Gryffindor +2")
-    else:
-        print("Respuesta invalida")
-Q2()
-def Q3():
-    print ("Cual tipo de instrumento musical te gusta escuchar?:")
-    print ("1. El violin")
-    print ("2. La trompeta")
-    print ("3. El piano")
-    print ("4. El tambor")
-    respuesta3=int(input("Su respuesta es:  "))
-    if respuesta3==1:
-        print("Hufflepuff +4")
-    elif respuesta3==2:
-        print("Slytherin +4")
-    elif respuesta3==3:
-        print("Ravenclaw +4")
-    elif respuesta3==4:
-        print("Gryffindor +4")
-    else:
-        print("Respuesta invalida")
-Q3()
+Gryffindor_house = 0
+Ravenclaw_house = 0
+Hufflepuff_house = 0
+Slyhterin_house = 0
 
+
+print ("Te gusta el amanecer o el anochecer?")
+print ("1. Amanecer")
+print ("2. Anochecer")
+respuesta1=int(input("Su respuesta es: "))
+if respuesta1==1:
+    Gryffindor_house +=1 
+    Ravenclaw_house +=1
+elif respuesta1==2:
+    Hufflepuff_house +=1
+    Slyhterin_house +=1
+else:
+    print("Respuesta invalida")
+
+print ("Cuando muera, quiero que la gente me recuerde como: ")
+print ("1. El bien")
+print ("2. El grande")
+print ("3. El sabio")
+print ("4. El audaz")
+respuesta2=int(input("Su respuesta es:  "))
+if respuesta2==1:
+    Hufflepuff_house +=2
+elif respuesta2==2:
+    Slyhterin_house +=2
+elif respuesta2==3:
+    Ravenclaw_house +=2
+elif respuesta2==4:
+    Gryffindor_house +=2
+else:
+    print("Respuesta invalida")
+
+print ("Cual tipo de instrumento musical te gusta escuchar?:")
+print ("1. El violin")
+print ("2. La trompeta")
+print ("3. El piano")
+print ("4. El tambor")
+respuesta3=int(input("Su respuesta es:  "))
+if respuesta3==1:
+    Hufflepuff_house +=4
+elif respuesta3==2:
+    Slyhterin_house +=4
+elif respuesta3==3:
+    Ravenclaw_house +=4
+elif respuesta3==4:
+    Gryffindor_house +=4
+else:
+    print("Respuesta invalida")
+
+winning_house = "" #Declaro esta variable vacia para ir llenandola a continuacion
+
+if Gryffindor_house > 0:
+    winning_house = "Gryffindor"
+elif Ravenclaw_house >0:
+    winning_house = "Ravenclaw"
+elif Slyhterin_house > 0:
+    winning_house="Slytherin"
+elif Hufflepuff_house >0:
+    winning_house="Hufflepuff"
+print ("Felicidades, la casa a la que tienes mas afinidas es " + winning_house + "!!")
